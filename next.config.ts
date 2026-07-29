496cda26ecd2af0732d3b81337f3f7172312049f
Injamamul Hoq Tamim <injamamulhoqtamim@gmail.comm>
2026-07-29 14:36:02 +0600

    message

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  turbopack: {
+    root: "D:/Zyntrix Lab Marketing/zyntrix-marketing",
+  },
 };
 
 export default nextConfig;
